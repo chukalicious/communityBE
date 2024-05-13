e0539973a525fbc353c569a9ab6535f6710dfd04
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-05-13 16:15:46 -0700

    chore: Add errorMiddleware to handle server errors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 8000
 const cors = require("cors")
+const { errorHandler } = require("./middleware/errorMiddleware")
 const connectDB = require("./config/db")    
 
 const router = require("./routes/userRoutes")
@@ -12,8 +13,10 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
 app.use(cors())
+app.use(errorHandler)
 
 app.use("/api/users", router)
+
 
 
 
